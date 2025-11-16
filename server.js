5c89dfeca4f9e50600af4b0d26394af2d93cfb58
GarySPS <layngelngel96@gmail.com>
2025-11-16 10:52:47 +0630

    Update allowedOrigins in server.js
    Removed old allowed origins and added new frontend URL.

--- server.js
+++ server.js
@@ -30,8 +30,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
   'http://localhost:3001',
-  'https://www.adminnovachain.link',
-  'https://adminnovachain.link'
+  'https://novachain2-admin-frontend.vercel.app''
 ];
 
 const corsOptions = {
